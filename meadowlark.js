160a1f7ef07037d2c99ec17271dbaa5ae20b1ec3
Nikita-Minov <nikitachryackhov555@gmail.com>
2021-03-28 21:36:41 +0300

    Code refactoring. Also added some tests for routes

---
+++ meadowlark.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const handlers = require('./lib/handlers');
+
+const port = process.env.PORT || 3001;
+const meadowlark = express();
+
+meadowlark.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+}));
+meadowlark.set('view engine', 'handlebars');
+
+meadowlark.use(express.static(__dirname + '/public'));
+
+meadowlark.get('/', handlers.home);
+
+meadowlark.get('/about', handlers.about);
+
+meadowlark.use(handlers.notFound);
+
+meadowlark.use(handlers.serverError);
+
+meadowlark.listen(port, () => console.log(
+    `Express запущен на https://localhost:${port};` +
+    `нажмите Ctr+C для завершения.`
+));
